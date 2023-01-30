de600fc9159c76e98060736a28df500aecfbdd7f
Xurshid <kaxudi34801@gmail.com>
2023-01-31 02:25:56 +0500

    5

--- homework__WarmUp1__codingBat.js
+++ homework__WarmUp1__codingBat.js
@@ -2,7 +2,19 @@ function sleepIn(weekday, vacation){
     return !weekday || vacation;
   }
 
-  function monkeyTrouble(aSmile, bSmile){
+function monkeyTrouble(aSmile, bSmile){
     return aSmile && bSmile || !aSmile && !bSmile ? true : false;
-  }
-  
+}
+
+function sumDouble(a, b){
+    return a == b ? (a + b) * 2 : a + b;
+}
+
+function diff21(n){
+    return n > 21 ? (n-21)*2 : 21 - n;
+}
+
+function parrotTrouble(talking, hour){
+    return talking && (hour < 7 || hour > 20);
+}
+
