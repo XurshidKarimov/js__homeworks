4c5883bf6e64b36800242030cd462fa06c782f93
Xurshid <kaxudi34801@gmail.com>
2023-01-31 03:29:25 +0500

    11

--- homework__WarmUp1__codingBat.js
+++ homework__WarmUp1__codingBat.js
@@ -18,3 +18,32 @@ function parrotTrouble(talking, hour){
     return talking && (hour < 7 || hour > 20);
 }
 
+function makes10(a, b){
+    return (a == 10 || b == 10) || a + b == 10;
+}
+
+function nearHundred(n){
+    let absolute = Math.abs(n)%10;
+    return absolute ? n >= 90 && n <= 100 || n >= 190 && n <= 200 : n ? true : false;
+}
+
+function posNeg(a, b, negative){
+    return (negative) ? a < 0 && b < 0 : (a > 0 && b < 0 || a < 0 && b > 0) ? true : false;
+}
+
+function notString(str){
+    return str.startsWith("not") ? str : `not ${str}`;
+}
+
+function missingChar(str, n){
+    str = str.split('');
+    str.splice(n, 1);
+  
+   return str.join(''); 
+}
+
+function frontBack(str){
+    let point = str.length * -1;
+    return str.length == 1 ? str : str.length == 2 ? `${str.slice(-1)}${str.slice(-2, -1)}` : `${str.slice(-1)}${str.slice(point+1, -1)}${str.slice(point, point+1)}`;
+}
+
