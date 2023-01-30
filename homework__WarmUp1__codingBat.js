ea7678bc13ff6f409fc93fad84bc2e498d9edf78
Xurshid <kaxudi34801@gmail.com>
2023-01-31 02:18:04 +0500

    starting warmup1

---
+++ homework__WarmUp1__codingBat.js
@@ -0,0 +1,8 @@
+function sleepIn(weekday, vacation){
+    return !weekday || vacation;
+  }
+
+  function monkeyTrouble(aSmile, bSmile){
+    return aSmile && bSmile || !aSmile && !bSmile ? true : false;
+  }
+  
