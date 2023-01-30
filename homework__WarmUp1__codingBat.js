944849acc9bfd0ea98f552758190a002139ea7ad
Xurshid <kaxudi34801@gmail.com>
2023-01-31 03:50:39 +0500

    ...

--- homework__WarmUp1__codingBat.js
+++ homework__WarmUp1__codingBat.js
@@ -47,3 +47,31 @@ function frontBack(str){
     return str.length == 1 ? str : str.length == 2 ? `${str.slice(-1)}${str.slice(-2, -1)}` : `${str.slice(-1)}${str.slice(point+1, -1)}${str.slice(point, point+1)}`;
 }
 
+function front3(str){
+    return str.slice(0, 3).repeat(3);
+}
+
+function backAround(str){
+    return `${str.slice(-1)}${str}${str.slice(-1)}`;
+}
+
+function or35(n){
+    return !(n%3) || !(n%5);
+}
+
+function front22(str){
+    return `${str.slice(0, 2)}${str}${str.slice(0, 2)}`;
+}
+
+function startHi(str){
+    return str.startsWith("hi");
+}
+
+function icyHot(temp1, temp2){
+    return (temp1 > 100 && temp2 < 0) || (temp2 > 100 && temp1 < 0);
+}
+
+function in1020(a, b){
+    return a >= 10 && a <= 20 || b >= 10 && b <= 20;
+}
+
